a39acd2a0c0e81b6e8639b86eef32155c5355b99
sg976 <sg9769356@gmail.com>
2023-08-27 03:25:06 +0530

    added favicon and seo stuff

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,17 @@
-import Link from 'next/link'
 import '@/styles/globals.css'
+import Head from 'next/head'
 import NavBar from '@/components/NavBar'
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta
+          name='description'
+          content='Buy and sell your favorite NFTs on our platform!'
+        />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </div>
